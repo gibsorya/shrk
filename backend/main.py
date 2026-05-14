166df5e31e265eb1ff0463a042dc4f2a96f1969c
Ryan Gibson <ryan@gibs.gg>
2026-05-14 15:05:49 -0500

    allow backend to connect to postgres db

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
+from backend.db import get_db
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
